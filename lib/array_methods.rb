eb79de25b50b1f5b9db9506aa5ec4c50b22f693b
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-06-28 14:53:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  array.length.times do |find_max_value|
-  array.max do
-    return max
+  array.max
 end
 
 def find_min_value(array)
